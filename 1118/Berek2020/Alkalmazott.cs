4b4ddf83e9712794bc105d59a086d28a45a075ef
BarcziBence <94312075+BarcziBence@users.noreply.github.com>
2024-11-18 10:02:25 +0100

    Add files via upload

---
+++ 1118/Berek2020/Alkalmazott.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berek2020
+{
+    internal class Alkalmazott
+    {
+        private string nev;
+        private bool nem;
+        private string reszleg;
+        private int belepes;
+        private int fizetes;
+
+        public string Nev { get => nev; set => nev = value; }
+        public bool Nem { get => nem; set => nem = value; }
+        public string Reszleg { get => reszleg; set => reszleg = value; }
+        public int Belepes { get => belepes; set => belepes = value; }
+        public int Fizetes { get => fizetes; set => fizetes = value; }
+
+        public Alkalmazott(string r)
+        {
+            var split = r.Split(';');
+            Nev = split[0];
+            Reszleg = split[2];
+            Belepes = int.Parse(split[3]);
+            Fizetes = int.Parse(split[4]);
+            Nem = true;
+            if (split[1] == "férfi") { Nem = false; }
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev} " +
+                (Nem ? "nő " : "férfi ")  +
+                $"{Reszleg} " +
+                $"{Belepes} " +
+                $"{Fizetes}";
+        }
+    }
+}
---
+++ 1118/Berek2020/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using Berek2020;
+
+List<Alkalmazott> alkalmazottak = new();
+FileStream fs = new FileStream("berek2020.txt", FileMode.Open);
+StreamReader sr = new StreamReader(fs);
+
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    alkalmazottak.Add(new Alkalmazott(sr.ReadLine()));
+}
+
+sr.Close();
+fs.Close();
+
+Console.WriteLine("3. feladat");
+Console.WriteLine(" A lista {0} alkalmazottat tartalmaz", alkalmazottak.Count);
+Console.WriteLine("\n");
+
+Console.WriteLine("4. feladat");
+var atlag = alkalmazottak.Average(a => a.Fizetes);
+Console.WriteLine("Az alkalmazottak átlag fizetése: {0:0.0}Eft", atlag/1000);
+Console.WriteLine("\n");
+
+Console.WriteLine("5. feladat");
+Console.Write("Adja meg egy részleg nevét: ");
+string keresett = Console.ReadLine();
+Console.WriteLine(keresett);
+Console.WriteLine("\n");
+
+Console.WriteLine("6. feladat");
+var max = alkalmazottak.Where(a => a.Reszleg == keresett).MaxBy(a => a.Fizetes);
+if(max != null) { Console.WriteLine("A keresett részlegen legnagyobb fizetéssel rendelkező alkalmazott: {0}", max); }
+else { Console.WriteLine("A keresett részleg nem létezik a cégnél"); }
+Console.WriteLine("\n");
+
+Console.WriteLine("7. feladat");
+var reszlegek = alkalmazottak.GroupBy(a => a.Reszleg);
+foreach(var a in reszlegek)
+{
+    Console.WriteLine("\t {0} - {1}", a.Key, a.Count());
+}
